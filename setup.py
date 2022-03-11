23780ef3c075e76bbbc9d99a6e650d6baae961cb
Steffen <steffen.albrecht@desy.de>
2022-03-11 14:03:28 +0100

    updated requirements with numpy

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 install_requires = [
     'scikit-image',
+    'numpy',
 ]
 
 setup( install_requires=install_requires, use_scm_version = lambda: {"local_scheme":lambda version: ""},)
